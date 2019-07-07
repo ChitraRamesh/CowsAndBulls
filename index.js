a8b985d4139b46c7992083ccb1d5a240663d2c50
ChitraRamesh <chitrasruthi@gmail.com>
2019-07-07 16:46:46 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -32,38 +32,41 @@ calculateCowAndBull = (word, saidWord) => {
     var arr_saidWord = saidWord.toUpperCase().split('')
     var bull = 0;
     var cow = 0;
-
+    var posOfHandledLetters =  word.toUpperCase().split('')
 
     for(i = 0; i < saidWord.length; i++) {
         var cowfound = false;
         for(j = 0; j < word.length; j++) {
+           //document.write (" <br>word " + arr_word[i]  + "said " + arr_saidWord[j])
             if(arr_word[i] == arr_saidWord[j]) {
-                if(i == j) //same position
-                {
-                bull++; 
-                cowfound = false;
-                break; 
+                if(i == j)  {//same position
+                
+                    bull++; 
+                    cowfound = false;
+                    posOfHandledLetters[j] = 1
+                    break; 
                 }
                 else {
-                cowfound = true;
+                if(posOfHandledLetters[j] != 1)
+                         cowfound = true;
                 }
                 
             }
                 
         }
     if (cowfound) cow++;  
-
+      
     }
     return [bull, cow];
 }
 
-processWords = (myWord, saidWord, lifespanCount,myContext, res) => {
+processWords = (sessionId, myWord, saidWord, lifespanCount,myContext, res) => {
     console.log(myWord, saidWord);
     myWord = myWord.toUpperCase();
     saidWord = saidWord.toUpperCase();
     //check if it is the same word
     if(myWord.indexOf(saidWord) == 0) {
-      client.del('myWord'); //reset the word
+      client.del(sessionId); //reset the word
        return res.json({
               fulfillmentText: myWord + " is right. If you want to play again, say the number of letters ",
               "outputContexts": [
@@ -82,7 +85,7 @@ processWords = (myWord, saidWord, lifespanCount,myContext, res) => {
     responseText = saidWord + " has " + bulls + " bulls and " + cows + " cows ";
     if(lifespanCount == undefined) 
     {
-      client.del('myWord');//reset the word
+      client.del(sessionId);//reset the word
       responseText += " No more attempts ";
     }
     else 
@@ -99,6 +102,7 @@ server.post('/get-cows-and-bulls', (req, res) => {
   
   //get the context readyState
   let countOfContexts = req.body.queryResult.outputContexts.length;
+  let sessionId = req.body.session
   let myContext  = "sayletter-followup";
   var lifespanCount = 0;
   var lengthOfWord = 0;
@@ -118,14 +122,14 @@ let saidWord = req.body.queryResult.parameters.theword;
 if(saidWord.length != lengthOfWord)  
 {
     responseText  = saidWord + " is not a " + lengthOfWord + " letter word"  
-    responseText += " You have " + lifespanCount + "attempts";
+    responseText += " You have " + lifespanCount + "attempts";          
     return res.json({
          
         fulfillmentText:  responseText         
     });
 }
 let myWord = "ERR" 
-myWord = client.get('myWord', function (error, result) {
+myWord = client.get(sessionId, function (error, result) {
     if (error || (result ==  null)) {
        //word not set yet
        // returns a random integer from 0 to 9]
@@ -134,7 +138,7 @@ myWord = client.get('myWord', function (error, result) {
              myWord = arrayOf3letterWords[Math.floor(Math.random() * 10)];   
        if(lengthOfWord == 4)     
             myWord = arrayOf4letterWords[Math.floor(Math.random() * 10)];     
-       client.set('myWord', myWord);
+       client.set(sessionId, myWord);
        console.log("My new word is " + myWord)
        
     }
@@ -144,14 +148,10 @@ myWord = client.get('myWord', function (error, result) {
        console.log("My already set word is " + myWord)
     }
 
-    return processWords(myWord, saidWord, lifespanCount,myContext,res)
+    return processWords(sessionId, myWord, saidWord, lifespanCount,myContext,res)
      
 });
-
-//let myWord = arrayOf3letterWords[Math.floor(Math.random() * 10)];     // returns a random integer from 0 to 9]
- 
-    
-     
+   
     
 });
 
